bb3310849eeb9a8e94cbf685d9058723e8ff1deb
panyamprasad <panyam.prasad2@gmail.com>
2026-08-10 09:44:02 +0530

    Self Introduction.

---
+++ 8_angular/interview.js
@@ -0,0 +1,24 @@
+const { application } = require("express")
+
+1. What is Angualr?
+    1. Angular is a development platform for building a single page applications for mobile and webkitURL.
+    2. It will used for building applications using HTML, CSS and a programming language like a JavaScript or TypeScript.
+    3. Angular is not a programming language it is a framework.
+     ---------------------------------------------------------    
+    Framework:
+        1. A framework is like platform for developing software applications.
+        2. Framework can have a pre-defined classes functions and methods, those are reusable for several functionalities.A
+        3. We don't need to write it from scratch by our own.
+
+2. What is Single Page application?
+    1. A single page application is a web application, it loads only one html page when application start.
+    2. After that navigation happens without reloading the entire page.
+    3. Frameworks like Angular use client side routing to display different types of components dynamically.A
+    4. For Example if we client Home to Product, Angular doesn't request a new HTML page from the server, instead of it's replace the home component to product component.
+    5. Inside the same page we can use the api's will fetch the data.
+    6. So this will be the faster and performance is good.
+    -------------------------------------------------------------
+
+3. Why should we use angular?
+    1. Angular gives clean and loosely coupled structured
+4. Different types of angular?
---
+++ self_Introduction.js
@@ -0,0 +1,24 @@
+
+// --> 1. Good morning, and thank you for giving me the opportunity to introduce myself.
+
+// --> 2. My name is **Panyam Vara Prasad**, and I have 9+ years of experience in IT, with around **6+ years of strong hands-on experience in backend development**, mainly working with **Node.js, JavaScript, TypeScript, NestJS and AWS services**.
+
+// --> 3. I started my career with web application development, where I worked with Angular, TypeScript.
+
+// --> 4. One of my key projects was Tresl, where I worked on Angular frontend development.
+
+// --> 5. After that, I moved to backend development. few of the projects, I worked mainly on Node.js APIs developing backend services and integrations.
+
+// --> 6. After that, I worked on the Victory Capital Management project, where my role became strongly focused on Node.js and AWS serverless development. 
+
+// --> 7. I developed scalable REST APIs using Node.js and worked with AWS Lambda, API Gateway, DynamoDB, Serverless Framework and microservices. I was also involved in unit testing using Mocha and Chai, CloudWatch monitoring, Git and Agile processes.
+
+// --> 8. Currently, I am working as a Senior Developer at Careator Technologies on the Insurance & Money Services project for Tesco Technologies.
+
+// --> 9. In my current project, I am mainly working with NestJS, Node.js, TypeScript, AWS, REST APIs and SOAP integrations.
+
+// --> 10. I am involved in enterprise application migration, backend service development, API and input validation, exception handling, structured logging, monitoring and production issue resolution.
+
+// --> 11. Overall, my career has started with product support  to backend development, with strong experience in Node.js, NestJS, AWS serverless services, REST APIs, DynamoDB and microservices.
+
+// That is a brief overview of my experience. Thank you.
